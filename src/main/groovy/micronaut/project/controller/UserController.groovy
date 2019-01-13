d5341eb54f9d8ec99ed823d0f3da64202adbbe71
unknown <prashantdcoder@gmail.com>
2019-01-12 23:28:10 -0800

    Delete user profile

--- src/main/groovy/micronaut/project/controller/UserController.groovy
+++ src/main/groovy/micronaut/project/controller/UserController.groovy
@@ -1,18 +1,17 @@
 package micronaut.project.controller
 
-import io.micronaut.context.annotation.Bean
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import micronaut.project.CO.AddCO
-import micronaut.project.CO.UpdateCO
+import micronaut.project.CO.*
 import micronaut.project.DTO.DTO
 import micronaut.project.service.UserService
 import javax.inject.Inject
 import javax.validation.Valid
+import javax.validation.constraints.NotEmpty
 
 @Controller("/user")
 class UserController {
@@ -26,18 +25,23 @@ class UserController {
     }
 
     @Post(value = "/add", consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    HttpResponse<DTO> add(@Valid AddCO addCO){
+    HttpResponse<DTO> add(@Valid AddCO addCO) {
         return HttpResponse.ok(userService.add(addCO))
     }
 
-    @Post(value = "/edit",consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    HttpResponse<DTO> edit(String userId){
+    @Post(value = "/edit", consumes = MediaType.APPLICATION_FORM_URLENCODED)
+    HttpResponse<DTO> edit(String userId) {
         return HttpResponse.ok(userService.edit(userId))
     }
 
     @Post(value = "/update", consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    HttpResponse<DTO> update(@Body @Valid UpdateCO updateCO){
+    HttpResponse<DTO> update(@Body @Valid UpdateCO updateCO) {
         return HttpResponse.ok(userService.update(updateCO))
+    }
+
+    @Post(value = "/delete", consumes = MediaType.APPLICATION_FORM_URLENCODED)
+    HttpResponse<DTO> delete(@NotEmpty String userId) {
+        return HttpResponse.ok(userService.delete(userId))
     }
 
 }
